3ca1ee5409b9bd59cdd0ab98b6d40ec7c2c37b60
Case <90377400+CaseIRL@users.noreply.github.com>
2023-10-29 05:58:01 +0000

    Update config.lua
    Added rep stuff

--- server/config.lua
+++ server/config.lua
@@ -15,6 +15,25 @@ config_sv.groups = {
     max_members = 6, -- Default max amount of members who can join a group
 }
 
+-- Reputation settings
+config_sv.reputation = {
+    sql = {
+        ['table_name'] = 'player_reputation'
+    },
+    reputation_list = {
+        ['garbage'] = { 
+            ['id'] = 'garbage', -- ID used to match reputation
+            ['category'] = 'civilian_job',  -- Category used to categorize different reputation types
+            ['label'] = 'Garbage Collection', -- Human readable label for the reputation
+            ['level'] = 1, -- Level players will start at
+            ['start_rep'] = 0, -- Amount of rep players will start with
+            ['first_level_rep'] = 1000, -- Amount of rep required for the first level
+            ['growth_factor'] = 1.5, -- Growth factor to increase rep per level based on first level rep default; level 1 -> 2 = 1000rep, 2 -> 3 = 1500rep, 3 -> 4 = 2250rep, 4 -> 5 = 3375rep.. and so on
+            ['max_level'] = 10  -- Max level players can achieve for reputation
+        }
+    }
+}
+
 -- Skills settings
 config_sv.skills = {
     sql = {
