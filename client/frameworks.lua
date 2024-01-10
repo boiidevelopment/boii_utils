d6a0ccf15a643ceb8aa2c2bc23fddafd7e000a0a
Case <90377400+CaseIRL@users.noreply.github.com>
2024-01-10 12:16:23 +0000

    Update frameworks.lua
    -> Edited get_player_name function now returns both first + last name as separate params
    -> Added get_identity function same as server side

--- client/frameworks.lua
+++ client/frameworks.lua
@@ -25,6 +25,7 @@ end
     FUNCTIONS
 ]]
 
+-- Function to get a players data
 local function get_data(key)
     local player_data
     if framework == 'boii_base' then
@@ -42,22 +43,60 @@ local function get_data(key)
     end
 end
 
+-- Function to return a players name
 local function get_player_name()
-    local player_data = get_data()
-    local player_name
+    local p_data = get_data()
+    if not p_data then return false end
+    local first_name, lastname
     
     if framework == 'boii_base' then
-        player_name = player_data.identity.first_name .. ' ' .. player_data.identity.last_name
-        return player_name
+        first_name = p_data.identity.first_name
+        last_name = p_data.identity.last_name
+        return first_name, last_name
     elseif framework == 'qb-core' then
-        player_name = player_data.charinfo.firstname .. ' ' .. player_data.charinfo.lastname
-        return player_name
+        first_name = p_data.charinfo.firstname
+        last_name = p_data.charinfo.lastname
+        return first_name, last_name
     elseif framework == 'esx_legacy' then
         -- TO DO:
     elseif framework == 'ox_core' then  
         -- TO DO:
     elseif framework == 'custom' then
         -- add code for your own framework here
+    end
+end
+
+-- Function to get a players identity information depending on framework
+local function get_identity(_src)
+    local p_data = get_data()
+    if not p_data then return false end
+
+    local player_data
+
+    if framework == 'boii_base' then
+        player_data = {
+            first_name = p_data.identity.first_name,
+            last_name = p_data.identity.last_name,
+            dob = p_data.identity.dob,
+            sex = p_data.identity.sex,
+            nationality = p_data.identity.nationality
+        }
+        return player_data
+    elseif framework == 'qb-core' then
+        player_data = {
+            first_name = p_data.charinfo.firstname,
+            last_name = p_data.charinfo.lastname,
+            dob = p_data.charinfo.birthdate,
+            sex = p_data.charinfo.gender,
+            nationality = p_data.charinfo.nationality
+        }
+        return player_data
+    elseif framework == 'esx_legacy' then
+        -- TO DO:
+    elseif framework == 'ox_core' then  
+        -- TO DO:
+    elseif framework == 'custom' then
+        -- Add your own custom framework code here
     end
 end
 
