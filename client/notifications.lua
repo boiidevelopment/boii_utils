10656fed1eb4044f6da4870ce3097f58e81457e4
Case <90377400+CaseIRL@users.noreply.github.com>
2024-01-14 13:10:52 +0000

    Update notifications.lua
    - Notifications
    -> Fixed issue with source being passed as first param on client side causing notifications to fail
    -> Added qb-core notifications
    -> Added esx_legacy notifications

--- client/notifications.lua
+++ client/notifications.lua
@@ -8,11 +8,22 @@
 
 local notifications = config.notifications
 
-local function send(_src, options)
+local function send(options)
     if notifications == 'boii_ui' then
         TriggerEvent('boii_ui:notify', options.type, options.header, options.message, options.duration)
+    elseif notifications == 'qb-core' then
+        if options.type == 'information' then
+            options.type = 'primary'
+        end
+        TriggerEvent('QBCore:Notify', options.message, options.type, options.duration)
+    elseif notifications == 'esx_legacy' then
+        if options.type == 'information' then
+            options.type = 'info'
+        end
+        TriggerEvent("ESX:Notify", options.type, options.duration, options.message)
     end
 end
+
 
 --[[
     ASSIGN LOCALS
