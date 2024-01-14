4a868b318209a514b88f6a3d075bfecdd0cd82ad
Case <90377400+CaseIRL@users.noreply.github.com>
2024-01-14 13:17:20 +0000

    Update frameworks.lua
    - Frameworks
    -> Added esx_legacy stuff to functions

--- client/frameworks.lua
+++ client/frameworks.lua
@@ -8,6 +8,7 @@
 
 framework = config.framework
 
+-- Framework initialization
 if framework == 'boii_base' then
     fw = exports['boii_base']:get_object()
 elseif framework == 'qb-core' then
@@ -15,17 +16,17 @@ elseif framework == 'qb-core' then
 elseif framework == 'esx_legacy' then
     fw = exports['es_extended']:getSharedObject()
 elseif framework == 'ox_core' then    
-    -- TO DO:
+    -- TO DO: Add initialization for ox_core
 elseif framework == 'custom' then
-    -- add code for your own framework here
+    -- Custom framework initialization
 end
-
 
 --[[
     FUNCTIONS
 ]]
 
--- Function to get a players data
+-- Function to get and return a player's client-side data
+-- Usage: local data = utils.fw.get_data('key') -- Using a key is not required
 local function get_data(key)
     local player_data
     if framework == 'boii_base' then
@@ -43,38 +44,44 @@ local function get_data(key)
     end
 end
 
--- Function to get a players identity information depending on framework
+-- Function to get a player's identity information depending on the framework
+-- Usage: local identity = utils.fw.get_identity()
 local function get_identity()
-    local p_data = get_data()
-    if not p_data then return false end
+    local player = get_data()
+    if not player then return false end
 
     local player_data
 
     if framework == 'boii_base' then
         player_data = {
-            first_name = p_data.identity.first_name,
-            last_name = p_data.identity.last_name,
-            dob = p_data.identity.dob,
-            sex = p_data.identity.sex,
-            nationality = p_data.identity.nationality
+            first_name = player.identity.first_name,
+            last_name = player.identity.last_name,
+            dob = player.identity.dob,
+            sex = player.identity.sex,
+            nationality = player.identity.nationality
         }
-        return player_data
     elseif framework == 'qb-core' then
         player_data = {
-            first_name = p_data.charinfo.firstname,
-            last_name = p_data.charinfo.lastname,
-            dob = p_data.charinfo.birthdate,
-            sex = p_data.charinfo.gender,
-            nationality = p_data.charinfo.nationality
+            first_name = player.charinfo.firstname,
+            last_name = player.charinfo.lastname,
+            dob = player.charinfo.birthdate,
+            sex = player.charinfo.gender,
+            nationality = player.charinfo.nationality
         }
-        return player_data
     elseif framework == 'esx_legacy' then
-        -- TO DO:
+        player_data = {
+            first_name = fw.PlayerData.firstName,
+            last_name = fw.PlayerData.lastName,
+            dob = fw.PlayerData.dateofbirth,
+            sex = fw.PlayerData.sex,
+            nationality = 'LS, Los Santos'
+        }
     elseif framework == 'ox_core' then  
         -- TO DO:
     elseif framework == 'custom' then
         -- Add your own custom framework code here
     end
+    return player_data
 end
 
 --[[
