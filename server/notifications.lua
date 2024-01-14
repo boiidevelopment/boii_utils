3feb1a78df057dcecff5eb9bf65365d4fad4b431
Case <90377400+CaseIRL@users.noreply.github.com>
2024-01-14 13:09:41 +0000

    Update notifications.lua
    - Notifications
    -> Added qb-core notifications
    -> Added esx_legacy notifications

--- server/notifications.lua
+++ server/notifications.lua
@@ -11,6 +11,16 @@ local notifications = config.notifications
 local function send(_src, options)
     if notifications == 'boii_ui' then
         TriggerClientEvent('boii_ui:notify', _src, options.type, options.header, options.message, options.duration)
+    elseif notifications == 'qb-core' then
+        if options.type == 'information' then
+            options.type = 'primary'
+        end
+        TriggerClientEvent('QBCore:Notify', _src, options.message, options.type, options.duration)
+    elseif notifications == 'esx_legacy' then
+        if options.type == 'information' then
+            options.type = 'info'
+        end
+        TriggerClientEvent("ESX:Notify", _src, options.type, options.duration, options.message)
     end
 end
 
