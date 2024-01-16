a746c8eb8942d177a63c28e502736af8c0f505f0
Case <90377400+CaseIRL@users.noreply.github.com>
2024-01-16 02:39:19 +0000

    Update config.lua

--- client/config.lua
+++ client/config.lua
@@ -8,6 +8,7 @@ utils.callbacks = {}
 
 -- Framework settings
 config.framework = 'boii_base' -- Choose your framework here. Available options; 'boii_base', 'qb-core', 'ox_core', 'esx_legacy', 'custom': For custom frameworks you need to edit the included frameworks.lua files.
+config.notifications = 'boii_ui' -- Choose your notifications here; Available options; 'boii_ui', 'qb-core', 'ox_lib', 'custom': For notification scripts not listed by default you can fill in your own in the notifications.lua files
 
 -- Debug settings
 config.debug = {
