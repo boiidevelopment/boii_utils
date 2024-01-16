b2f45e23a294cfede27f15fc6dd16f2da031de37
Case <90377400+CaseIRL@users.noreply.github.com>
2024-01-16 02:43:24 +0000

    Update config.lua

--- server/config.lua
+++ server/config.lua
@@ -8,17 +8,14 @@ utils.callbacks = {}
 
 -- Framework settings
 config.framework = 'boii_base' -- Choose your framework here. Available options; 'boii_base', 'qb-core', 'ox_core', 'esx_legacy', 'custom': For custom frameworks you need to edit the included frameworks.lua files.
-
--- SQL settings
-config.sql = {
-    wrapper = 'oxmysql', -- Available options: 'oxmysql', 'mysql-async', 'ghmattimysql', 'custom'
-}
+config.notifications = 'boii_ui' -- Choose your notifications here; Available options; 'boii_ui', 'qb-core', 'ox_lib', 'custom': For notification scripts not listed by default you can fill in your own in the notifications.lua files
 
 -- Debug settings
 config.debug = {
     reputation = true, -- Toggle debug prints for reputation system
     skills = true, -- Toggle debug prints for skill system
-    licence = true -- Toggle debug prints for licence system
+    licence = true, -- Toggle debug prints for licence system
+    conversions = true -- Toggle debug prints for data conversions
 }
 
 -- Group settings
