26f0b0ee2a7bc8b7e6af9eb1e1cc8d3be08cb3b6
Case <90377400+CaseIRL@users.noreply.github.com>
2024-01-26 16:09:26 +0000

    Delete shared directory

--- shared/exports.lua
+++
@@ -1,9 +0,0 @@
-----------------------------------
---<!>-- BOII | DEVELOPMENT --<!>--
-----------------------------------
-
--- Export to get all utils
--- local utils = exports['boii_utils']:get_utils()
-exports('get_utils', function() 
-    return utils.tables.deep_copy(utils) 
-end)
