6c067f9303f9dff6e94d51691c6314e759a0d40e
Case <90377400+CaseIRL@users.noreply.github.com>
2024-07-28 14:57:35 +0100

    Update notifications.lua

--- server/scripts/bridges/notifications.lua
+++ server/scripts/bridges/notifications.lua
@@ -12,31 +12,30 @@
 --- Notification system.
 --- @script client/scripts/bridges/notifications.lua
 
+local NOTIFICATIONS = config.ui.notify
+
 --- @section Local functions
 
 --- Notification bridge function.
 --- @function notify
 --- @param options table: The notification options (type, message, header, duration).
 local function notify(_src, options)
-    if not options or not options.type or not options.message then
-        print('Invalid notification data provided')
-        return
-    end
+    if not options or not options.type or not options.message then print('Invalid notification data provided') return end
 
     if NOTIFICATIONS == 'boii_ui' then
-        TriggerEvent('boii_ui:notify', _src, { type = options.type, header = options.header, message = options.message, duration = options.duration })
+        TriggerClientEvent('boii_ui:notify', _src, { type = options.type, header = options.header, message = options.message, duration = options.duration })
     elseif NOTIFICATIONS == 'ox_lib' then
-        TriggerEvent('ox_lib:notify', _src, { type = options.type, title = options.header, description = options.message })
+        TriggerClientEvent('ox_lib:notify', _src, { type = options.type, title = options.header, description = options.message })
     elseif NOTIFICATIONS == 'es_extended' then
-        TriggerEvent('ESX:Notify', _src, options.type, options.duration, options.message)
+        TriggerClientEvent('ESX:Notify', _src, options.type, options.duration, options.message)
     elseif NOTIFICATIONS == 'qb-core' then
         local type_mapping = { information = 'primary', info = 'primary' }
         options.type = type_mapping[options.type] or options.type
-        TriggerEvent('QBCore:Notify', _src, options.message, options.type, options.duration)
+        TriggerClientEvent('QBCore:Notify', _src, options.message, options.type, options.duration)
     elseif NOTIFICATIONS == 'okokNotify' then
-        TriggerEvent('okokNotify:Alert', _src, options.header or 'Notification', options.message, options.type, options.duration or 5000)
+        TriggerClientEvent('okokNotify:Alert', _src, options.header or 'Notification', options.message, options.type, options.duration or 5000)
     else
-        TriggerEvent('boii_ui:notify', _src, { type = options.type, header = options.header, message = options.message, duration = options.duration })
+        TriggerClientEvent('boii_ui:notify', _src, { type = options.type, header = options.header, message = options.message, duration = options.duration })
     end
 end
 
